b4d5f2ddac95d26c37f84ead76c812c80b8e4ef7
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-19 13:06:10 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="card footer">
+      <h2>GitInfo | Developed by Pankaj Vaishnav React Batch Geekster | &copy; 2022</h2>
+      <a href="https://github.com/Abhishekjoshi98/" target="_blank" rel="noreferrer">Github Source Code</a>
+      <p> Made with ❤️ &amp; 💻</p>
+    </div>
+  )
+}
+
+export default Footer;
